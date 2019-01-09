9c37a29dc087d764554b505a15f155f3d1a1aa15
Reury araujo <Reury@users.noreply.github.com>
2019-01-09 19:18:47 -0300

    terminado estrutura sequencial

--- ex_EstruturaSequencial/04.py
+++ ex_EstruturaSequencial/04.py
@@ -1,2 +1,7 @@
-notas[4]
-bimestres[2]
+n1 = float(input("Digite o resultado da primeira prova:\n"))
+n2 = float(input("Digite o resultado da segunda prova:\n"))
+n3 = float(input("Digite o resultado da terceira prova:\n"))
+n4 = float(input("Digite o resultado da quarta prova:\n"))
+media = (n1 + n2 + n3 + n4) /4
+
+print("a media foi ", media)
